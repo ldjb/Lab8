75be590fb36a7e6530d59a8963b8ac41b75f4eb2
Leon Byford <leonbyford@138-38-151-244.eduroam.bath.ac.uk>
2012-12-01 17:08:20 +0000

    [02-COMPLETE] test example

--- ldjb20-Lab8/Lab8-02/Lab802Test.java
+++ ldjb20-Lab8/Lab8-02/Lab802Test.java
@@ -1,6 +1,15 @@
 public class Lab802Test {
 	
 	public static void main(String[] args) {
+	
+		for (int i = 0; i < 5; i++) {
+			Circle c = new Circle();
+			System.out.println("New Circle created with InstanceID " +
+				Integer.toString(c.getInstanceID()) + ".");
+		}
+		System.out.println("In total, there are "
+			+ Integer.toString(Circle.getNumInstances()) + " Circles.");
+	
 	}
 	
 }
