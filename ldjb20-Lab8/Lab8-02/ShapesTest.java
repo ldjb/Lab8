dd858cb29bd1a6a3b8cf93b02b659f85769af8a0
Leon Byford <leonbyford@138-38-151-244.eduroam.bath.ac.uk>
2012-12-01 16:45:19 +0000

    [02] add dependencies

---
+++ ldjb20-Lab8/Lab8-02/ShapesTest.java
@@ -0,0 +1,11 @@
+public class ShapesTest
+{
+   public static void main(String[] args)
+   {
+        Canvas c = Canvas.getCanvas();
+        Circle c1 = new Circle();
+        Circle c2 = new Circle();
+        c1.makeVisible();
+        c2.makeVisible();
+   }
+}
