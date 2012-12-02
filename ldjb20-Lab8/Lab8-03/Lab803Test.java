f7b01f147c6d53e7d7d91f2809d54ae5d84f66bc
Leon Byford <leonbyford@138-38-151-244.eduroam.bath.ac.uk>
2012-12-02 07:16:38 +0000

    [03-COMPLETE] test class

--- ldjb20-Lab8/Lab8-03/Lab803Test.java
+++ ldjb20-Lab8/Lab8-03/Lab803Test.java
@@ -2,10 +2,19 @@ public class Lab803Test {
 	
 	public static void main(String[] args) {
 	
+		CircleList clist = new CircleList();
+		System.out.println("* Add Circle:");
+		clist.add(new Circle());
+		System.out.println("* Add Circle:");
+		clist.add(new Circle());
 		Circle c = new Circle();
-		c.toString();
 		c.changeColor("red");
-		c.toString();
+		System.out.println("* Add Circle at specified index:");
+		clist.add(1, c);
+		System.out.println("* Remove Circle at specified index:");
+		clist.remove(0);
+		System.out.println("* Clear the Vector:");
+		clist.clear();
 	
 	}
 	
