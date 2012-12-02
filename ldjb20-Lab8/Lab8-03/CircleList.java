0372ffdf2de8b90d8459534995245d01ba1f57e4
Leon Byford <leonbyford@138-38-151-244.eduroam.bath.ac.uk>
2012-12-02 06:43:21 +0000

    [03] printDetails()

--- ldjb20-Lab8/Lab8-03/CircleList.java
+++ ldjb20-Lab8/Lab8-03/CircleList.java
@@ -10,18 +10,31 @@ public class CircleList {
 	
 	public void add(Circle c) {
 		clist.add(c);
+		printDetails();
 	}
 	
 	public void add(int index, Circle c) {
 		clist.add(index, c);
+		printDetails();
 	}
 	
 	public void remove(int index) {
 		clist.remove(index);
+		printDetails();
 	}
 	
 	public void clear() {
 		clist.clear();
+		printDetails();
+	}
+	
+	public void printDetails() {
+		int i = 0;
+		for (Circle c: clist) {
+			System.out.print("[" + Integer.toString(i) + "] ");
+			c.toString();
+			i++;
+		}
 	}
 
 }
