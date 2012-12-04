af49d4580a6ee6d832db00ec454550fc714e7adf
Leon Byford <leonbyford@138-38-151-244.eduroam.bath.ac.uk>
2012-12-04 02:44:08 +0000

    [05-COMPLETE] Done.
    IT'S ALL OVER!!!!! …Or at least, I think so. I hope so.
    
    *flops onto bed*

--- ldjb20-Lab8/Lab8-05/Lab805Test.java
+++ ldjb20-Lab8/Lab8-05/Lab805Test.java
@@ -1,9 +1,25 @@
 public class Lab805Test {
 	
+	private static boolean isPrime(int n) {
+		if (n > 1) {
+			for (int i=2; i<n; i++) {
+				if (n%i == 0) {
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+	
 	public static void main(String[] args) {
-	
-		
-	
+		for (int i=-5; i<21; i++) {
+			System.out.print(Integer.toString(i) + " is ");
+			if (!isPrime(i)) {
+				System.out.print("not ");
+			}
+			System.out.println("prime.");
+		}
 	}
 	
 }
